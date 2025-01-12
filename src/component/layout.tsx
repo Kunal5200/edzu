57671dc8cd2a80345b3636ed0b76d2ef2c10c9df
Kunal Sharma <112200812+Kunal5200@users.noreply.github.com>
2025-01-12 20:24:49 +0530

    add about us page and responsive design

--- src/component/layout.tsx
+++ src/component/layout.tsx
@@ -5,13 +5,14 @@ import Header from "./header";
 import Footer from "./footer";
 import { useMediaQuery } from "@mui/material";
 import MobileHeader from "./mobileHeader";
-
+import { Analytics } from "@vercel/analytics/react";
 const Layout = ({ children }: LayoutProps) => {
   const phone = useMediaQuery("(max-width:600px)");
   return (
     <div>
       <SubHeader />
       {phone ? <MobileHeader /> : <Header />}
+      <Analytics />
       {children}
       <Footer />
     </div>
